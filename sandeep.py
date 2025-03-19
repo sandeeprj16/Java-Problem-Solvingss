4c6de486d105a4442055d7b13eb9b876024dae1e
Sandeep <sj537376@gmail.com>
2025-03-20 01:34:20 +0530

    palidrome

--- sandeep.py
+++ sandeep.py
@@ -1,1 +1,17 @@
-print("sandeel is greata")
+print("sandeep is great")
+s = "malayalam"  # string
+
+i,j = 0, len(s) - 1  # two pointers
+
+is_palindrome = True  # assume palindrome
+while i < j:
+    if s[i] != s[j]:  # mismatch found
+        is_palindrome = False
+        break
+    i += 1
+    j -= 1
+
+if is_palindrome:
+    print("Yes") 
+else:
+    print("No")   
